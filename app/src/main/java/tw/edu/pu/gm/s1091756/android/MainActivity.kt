6c53b3fdd614e954607f4706d423c190f62ec5f6
chiayen02 <vivian910222@gmail.com>
2022-05-21 23:43:31 +0800

    v.3 Glide與Chain(個人照與按鈕綁在一起)

--- app/src/main/java/tw/edu/pu/gm/s1091756/android/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1091756/android/MainActivity.kt
@@ -5,13 +5,18 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
     lateinit var img : ImageView
     lateinit var game : MySurfaceView
     var flag:Boolean = false
     lateinit var job : Job
+    lateinit var circle : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         img = findViewById(R.id.img)
         game = findViewById(R.id.mysv)
+
+        circle = findViewById(R.id.circle)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.me)
+            .circleCrop()
+            .override(800, 600)
+            .into(circle)
 
         img.setOnClickListener({
             if (flag){
